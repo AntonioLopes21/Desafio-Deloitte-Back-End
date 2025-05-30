aa31b1c88d08acf17ec84dec5d682fd24507a34a
bootcamp02 <bootcamp02@deloitte.com>
2025-05-30 17:10:19 -0300

    criando enums e entidade agendamento

---
+++ src/main/java/com/usuarios/api_crud_usuarios/model/entity/Agendamento.java
@@ -0,0 +1,7 @@
+package com.usuarios.api_crud_usuarios.model.entity;
+
+import com.usuarios.api_crud_usuarios.enums.StatusAgendamento;
+
+public class Agendamento {
+    private StatusAgendamento statusDoAgendamento;
+}
