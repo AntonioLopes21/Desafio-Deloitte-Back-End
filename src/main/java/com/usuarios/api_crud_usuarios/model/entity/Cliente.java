36fa6017ef98831a00a0c525edffc4ab3b07d37d
bootcamp01 <bootcamp01@deloitte.com>
2025-05-30 17:40:42 -0300

    criação do cliente

--- src/main/java/com/usuarios/api_crud_usuarios/model/entity/Cliente.java
+++ src/main/java/com/usuarios/api_crud_usuarios/model/entity/Cliente.java
@@ -1,4 +1,29 @@
 package com.usuarios.api_crud_usuarios.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String senha;
+    private String telefone;
+
+    @OneToMany
+    @JsonIgnore
+    private Agendamento agendamento;
+
+
 }
