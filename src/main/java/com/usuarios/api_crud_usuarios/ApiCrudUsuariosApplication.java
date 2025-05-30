ebbdbde1c0e08712fbb1d64fcefc90c79d0e1b7f
bootcamp02 <bootcamp02@deloitte.com>
2025-05-30 16:33:53 -0300

    subindo estrutura de projeto inicial

---
+++ src/main/java/com/usuarios/api_crud_usuarios/ApiCrudUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.usuarios.api_crud_usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCrudUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usuarios/api_crud_usuarios/ApiCrudUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usuarios.api_crud_usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
